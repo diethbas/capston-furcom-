6ad24805be8e7abacd68779679c744033a101a84
diethbas <dietherbas27@gmail.com>
2024-10-29 14:40:37 +0800

    save all

--- config/database.php
+++ config/database.php
@@ -46,8 +46,8 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
+            'host' => env('DB_HOST', '5.tcp.ngrok.io'),
+            'port' => env('DB_PORT', '25019'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import fs from 'fs';
+import path from 'path';
 
 export default defineConfig({
+    server: {
+        https: {
+        key: fs.readFileSync(path.resolve(__dirname, 'localhost.key')),
+        cert: fs.readFileSync(path.resolve(__dirname, 'localhost.crt')),
+        },
+        host: 'localhost',
+        port: 443, 
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
