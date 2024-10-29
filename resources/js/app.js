3df0a1128d3648395bf74fc1eb07695f2067fef7
diethbas <dietherbas27@gmail.com>
2024-10-29 15:24:29 +0800

    build

--- resources/js/app.js
+++ resources/js/app.js
@@ -17,47 +17,49 @@ import Pusher from 'pusher-js';
 //     }
 // });
 
-const userId = window.Laravel.userId;
+const userId = (window.Laravel && window.Laravel.userId) || null;
 
-window.Laravel.showModal= function(id, firstname, lastname) {
-    if (window.Modal){
-        const modal2 = new window.Modal(document.getElementById('messageModal'));
-        modal2.show();
-        var dataEvent = new CustomEvent('threadShow', {});
-        
-        document.getElementById('sendMessage-ToName').innerHTML = firstname + ' ' + lastname;
-        document.getElementById('messageModal').dataset.idTo = id;
-        document.getElementById('messageModal').dispatchEvent(dataEvent);
-
-    }
-}
-
-Pusher.logToConsole = true;
-
-var pusher = new Pusher(`${import.meta.env.VITE_PUSHER_APP_KEY}`, {
-    cluster: `${import.meta.env.VITE_PUSHER_APP_CLUSTER}`,
-    authEndpoint: '/pusher/auth',
-    auth: {
-        headers: {
-            'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+if (userId){
+    window.Laravel.showModal= function(id, firstname, lastname) {
+        if (window.Modal){
+            const modal2 = new window.Modal(document.getElementById('messageModal'));
+            modal2.show();
+            var dataEvent = new CustomEvent('threadShow', {});
+            
+            document.getElementById('sendMessage-ToName').innerHTML = firstname + ' ' + lastname;
+            document.getElementById('messageModal').dataset.idTo = id;
+            document.getElementById('messageModal').dispatchEvent(dataEvent);
+    
         }
     }
-});
-
-var channel = pusher.subscribe(`private-chat.${userId}`);
-channel.bind('pusher:subscription_succeeded', () => {
-    console.log('Successfully subscribed to private-chat.4');
-});
-
-channel.bind('pusher:subscription_error', (status) => {
-    console.error('Subscription failed:', status);
-});
-console.log(channel);
-channel.bind('App\\Events\\MessageSent', function(data) {
-    console.log(`Message sent`);
-    window.Laravel.showModal(data.senderID, data.firstname, data.lastname);
-    console.log(`Message from ${data.senderID}: ${data.message}`);
-});
+    
+    Pusher.logToConsole = true;
+    
+    var pusher = new Pusher(`${import.meta.env.VITE_PUSHER_APP_KEY}`, {
+        cluster: `${import.meta.env.VITE_PUSHER_APP_CLUSTER}`,
+        authEndpoint: '/pusher/auth',
+        auth: {
+            headers: {
+                'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+            }
+        }
+    });
+    
+    var channel = pusher.subscribe(`private-chat.${userId}`);
+    channel.bind('pusher:subscription_succeeded', () => {
+        console.log('Successfully subscribed to private-chat.4');
+    });
+    
+    channel.bind('pusher:subscription_error', (status) => {
+        console.error('Subscription failed:', status);
+    });
+    console.log(channel);
+    channel.bind('App\\Events\\MessageSent', function(data) {
+        console.log(`Message sent`);
+        window.Laravel.showModal(data.senderID, data.firstname, data.lastname);
+        console.log(`Message from ${data.senderID}: ${data.message}`);
+    });
+}
 
 
 // window.Echo.private(`chat.${userId}`)
